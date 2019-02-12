f915a6b22438a9f4378d7f17548edc9727664b1c
Sunjay Varma <varma.sunjay@gmail.com>
2019-02-12 11:22:53 -0500

    Upgraded to latest versions of dependencies + some idiomatic changes

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,6 @@
 extern crate chrono;
 extern crate nom;
 
-use nom::IResult;
 use std::error::Error;
 
 use self::parser::patch;
@@ -17,29 +16,36 @@ pub use self::parser::{File, FileMetadata, Hunk, Line, Patch, Range};
 mod parser;
 
 #[derive(Debug)]
-pub enum PatchError {
-    ParseError,
+pub enum PatchError<'a> {
+    ParseError(nom::Err<&'a [u8]>),
 }
 
-impl std::fmt::Display for PatchError {
+impl<'a> From<nom::Err<&'a [u8]>> for PatchError<'a> {
+    fn from(err: nom::Err<&'a [u8]>) -> Self {
+        PatchError::ParseError(err)
+    }
+}
+
+impl<'a> std::fmt::Display for PatchError<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match *self {
-            PatchError::ParseError => write!(f, "Error while parsing"),
+        match self {
+            PatchError::ParseError(err) => write!(f, "Error while parsing: {}", err),
         }
     }
 }
 
-impl Error for PatchError {
+impl<'a> Error for PatchError<'a> {
     fn description(&self) -> &str {
-        match *self {
-            PatchError::ParseError => "parse error",
+        match self {
+            PatchError::ParseError(err) => err.description(),
         }
     }
 }
 
 pub fn parse(diff: &str) -> Result<Patch, PatchError> {
-    match patch(diff.as_bytes()) {
-        IResult::Done(_, p) => Ok(p),
-        IResult::Incomplete(_) | IResult::Error(_) => Err(PatchError::ParseError),
-    }
+    Ok(patch(diff.as_bytes()).map(|(remaining_input, patch)| {
+        // Parser should return an error instead of producing remaining input
+        assert!(remaining_input.is_empty(), "bug: failed to parse entire input");
+        patch
+    })?)
 }
--- src/parser.rs
+++ src/parser.rs
@@ -50,10 +50,10 @@ named!(non_escape<char>, none_of!("\\\"\0\n\r\t"));
 
 named!(
     escape<char>,
-    chain!(
-        tag!("\\") ~
-        c: one_of!("\\\"0nrtb"),
-        || c
+    do_parse!(
+        tag!("\\") >>
+        c: one_of!("\\\"0nrtb") >>
+        (c)
     )
 );
 
@@ -236,225 +236,229 @@ named!(pub patch<Patch>,
     )
 );
 
-#[test]
-fn test_unescape() {
-    assert_eq!(
-        unescape("file \\\"name\\\"".as_bytes()),
-        IResult::Done("".as_bytes(), "file \"name\"".to_string())
-    );
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[test]
-fn test_quoted() {
-    assert_eq!(
-        quoted("\"file name\"".as_bytes()),
-        IResult::Done("".as_bytes(), "file name".to_string())
-    );
-}
+    #[test]
+    fn test_unescape() {
+        assert_eq!(
+            unescape("file \\\"name\\\"".as_bytes()).unwrap(),
+            ("".as_bytes(), "file \"name\"".to_string())
+        );
+    }
 
-#[test]
-fn test_bare() {
-    assert_eq!(
-        bare("file-name ".as_bytes()),
-        IResult::Done(" ".as_bytes(), "file-name".to_string())
-    );
+    #[test]
+    fn test_quoted() {
+        assert_eq!(
+            quoted("\"file name\"".as_bytes()).unwrap(),
+            ("".as_bytes(), "file name".to_string())
+        );
+    }
 
-    assert_eq!(
-        bare("file-name\n".as_bytes()),
-        IResult::Done("\n".as_bytes(), "file-name".to_string())
-    );
-}
+    #[test]
+    fn test_bare() {
+        assert_eq!(
+            bare("file-name ".as_bytes()).unwrap(),
+            (" ".as_bytes(), "file-name".to_string())
+        );
 
-#[test]
-fn test_fname() {
-    assert_eq!(
-        fname("asdf ".as_bytes()),
-        IResult::Done(" ".as_bytes(), "asdf".to_string())
-    );
+        assert_eq!(
+            bare("file-name\n".as_bytes()).unwrap(),
+            ("\n".as_bytes(), "file-name".to_string())
+        );
+    }
 
-    assert_eq!(
-        fname("\"asdf\" ".as_bytes()),
-        IResult::Done(" ".as_bytes(), "asdf".to_string())
-    );
+    #[test]
+    fn test_fname() {
+        assert_eq!(
+            fname("asdf ".as_bytes()).unwrap(),
+            (" ".as_bytes(), "asdf".to_string())
+        );
 
-    assert_eq!(
-        fname("\"a s\\\"df\" ".as_bytes()),
-        IResult::Done(" ".as_bytes(), "a s\"df".to_string())
-    );
-}
+        assert_eq!(
+            fname("\"asdf\" ".as_bytes()).unwrap(),
+            (" ".as_bytes(), "asdf".to_string())
+        );
 
-#[test]
-fn test_header_line_contents() {
-    assert_eq!(
-        header_line_content("lao\n".as_bytes()),
-        IResult::Done(
-            "\n".as_bytes(),
-            File {
-                name: "lao".to_string(),
-                meta: None
-            }
-        )
-    );
+        assert_eq!(
+            fname("\"a s\\\"df\" ".as_bytes()).unwrap(),
+            (" ".as_bytes(), "a s\"df".to_string())
+        );
+    }
 
-    assert_eq!(
-        header_line_content("lao 2002-02-21 23:30:39.942229878 -0800\n".as_bytes()),
-        IResult::Done(
-            "\n".as_bytes(),
-            File {
-                name: "lao".to_string(),
-                meta: Some(FileMetadata::DateTime(
-                    DateTime::parse_from_rfc3339("2002-02-21T23:30:39.942229878-08:00").unwrap()
-                )),
-            }
-        )
-    );
-
-    assert_eq!(
-        header_line_content("lao 2002-02-21 23:30:39 -0800\n".as_bytes()),
-        IResult::Done(
-            "\n".as_bytes(),
-            File {
-                name: "lao".to_string(),
-                meta: Some(FileMetadata::DateTime(
-                    DateTime::parse_from_rfc3339("2002-02-21T23:30:39-08:00").unwrap()
-                )),
-            }
-        )
-    );
-
-    assert_eq!(
-        header_line_content("lao 08f78e0addd5bf7b7aa8887e406493e75e8d2b55\n".as_bytes()),
-        IResult::Done(
-            "\n".as_bytes(),
-            File {
-                name: "lao".to_string(),
-                meta: Some(FileMetadata::Other(
-                    "08f78e0addd5bf7b7aa8887e406493e75e8d2b55"
-                ))
-            }
-        )
-    );
-}
-
-#[test]
-fn test_headers() {
-    let sample = "\
---- lao 2002-02-21 23:30:39.942229878 -0800
-+++ tzu 2002-02-21 23:30:50.442260588 -0800\n"
-        .as_bytes();
-    assert_eq!(
-        headers(sample),
-        IResult::Done(
-            "".as_bytes(),
+    #[test]
+    fn test_header_line_contents() {
+        assert_eq!(
+            header_line_content("lao\n".as_bytes()).unwrap(),
             (
+                "\n".as_bytes(),
+                File {
+                    name: "lao".to_string(),
+                    meta: None
+                }
+            )
+        );
+
+        assert_eq!(
+            header_line_content("lao 2002-02-21 23:30:39.942229878 -0800\n".as_bytes()).unwrap(),
+            (
+                "\n".as_bytes(),
                 File {
                     name: "lao".to_string(),
                     meta: Some(FileMetadata::DateTime(
-                        DateTime::parse_from_rfc3339("2002-02-21T23:30:39.942229878-08:00")
-                            .unwrap()
-                    )),
-                },
-                File {
-                    name: "tzu".to_string(),
-                    meta: Some(FileMetadata::DateTime(
-                        DateTime::parse_from_rfc3339("2002-02-21T23:30:50.442260588-08:00")
-                            .unwrap()
+                        DateTime::parse_from_rfc3339("2002-02-21T23:30:39.942229878-08:00").unwrap()
                     )),
                 }
             )
-        )
-    );
+        );
 
-    let sample2 = "\
---- lao
-+++ tzu\n"
-        .as_bytes();
-    assert_eq!(
-        headers(sample2),
-        IResult::Done(
-            "".as_bytes(),
+        assert_eq!(
+            header_line_content("lao 2002-02-21 23:30:39 -0800\n".as_bytes()).unwrap(),
             (
+                "\n".as_bytes(),
                 File {
                     name: "lao".to_string(),
-                    meta: None,
-                },
-                File {
-                    name: "tzu".to_string(),
-                    meta: None,
+                    meta: Some(FileMetadata::DateTime(
+                        DateTime::parse_from_rfc3339("2002-02-21T23:30:39-08:00").unwrap()
+                    )),
                 }
             )
-        )
-    );
+        );
 
-    let sample3 = "\
---- lao 08f78e0addd5bf7b7aa8887e406493e75e8d2b55
-+++ tzu e044048282ce75186ecc7a214fd3d9ba478a2816\n"
-        .as_bytes();
-    assert_eq!(
-        headers(sample3),
-        IResult::Done(
-            "".as_bytes(),
+        assert_eq!(
+            header_line_content("lao 08f78e0addd5bf7b7aa8887e406493e75e8d2b55\n".as_bytes()).unwrap(),
             (
+                "\n".as_bytes(),
                 File {
                     name: "lao".to_string(),
                     meta: Some(FileMetadata::Other(
                         "08f78e0addd5bf7b7aa8887e406493e75e8d2b55"
-                    )),
-                },
-                File {
-                    name: "tzu".to_string(),
-                    meta: Some(FileMetadata::Other(
-                        "e044048282ce75186ecc7a214fd3d9ba478a2816"
-                    )),
+                    ))
                 }
             )
-        )
-    );
-}
+        );
+    }
 
-#[test]
-fn test_range() {
-    assert_eq!(
-        range("1,7".as_bytes()),
-        IResult::Done("".as_bytes(), Range { start: 1, count: 7 })
-    );
-
-    assert_eq!(
-        range("2".as_bytes()),
-        IResult::Done("".as_bytes(), Range { start: 2, count: 1 })
-    );
-}
-
-#[test]
-fn test_chunk_intro() {
-    let sample = "@@ -1,7 +1,6 @@\n".as_bytes();
-    assert_eq!(
-        chunk_intro(sample),
-        IResult::Done(
-            "".as_bytes(),
-            (Range { start: 1, count: 7 }, Range { start: 1, count: 6 },)
-        )
-    )
-}
-
-#[test]
-fn test_chunk() {
-    let sample = "\
-@@ -1,7 +1,6 @@
--The Way that can be told of is not the eternal Way;
--The name that can be named is not the eternal name.
- The Nameless is the origin of Heaven and Earth;
--The Named is the mother of all things.
-+The named is the mother of all things.
-+
- Therefore let there always be non-being,
-   so we may see their subtlety,
- And let there always be being,\n"
+    #[test]
+    fn test_headers() {
+        let sample = "\
+        --- lao 2002-02-21 23:30:39.942229878 -0800
+        +++ tzu 2002-02-21 23:30:50.442260588 -0800\n"
         .as_bytes();
-    let expected = Hunk {
-        old_range: Range { start: 1, count: 7 },
-        new_range: Range { start: 1, count: 6 },
-        lines: vec![
+        assert_eq!(
+            headers(sample).unwrap(),
+            (
+                "".as_bytes(),
+                (
+                    File {
+                        name: "lao".to_string(),
+                        meta: Some(FileMetadata::DateTime(
+                            DateTime::parse_from_rfc3339("2002-02-21T23:30:39.942229878-08:00")
+                            .unwrap()
+                        )),
+                    },
+                    File {
+                        name: "tzu".to_string(),
+                        meta: Some(FileMetadata::DateTime(
+                            DateTime::parse_from_rfc3339("2002-02-21T23:30:50.442260588-08:00")
+                            .unwrap()
+                        )),
+                    }
+                )
+            )
+        );
+
+        let sample2 = "\
+        --- lao
+        +++ tzu\n"
+        .as_bytes();
+        assert_eq!(
+            headers(sample2).unwrap(),
+            (
+                "".as_bytes(),
+                (
+                    File {
+                        name: "lao".to_string(),
+                        meta: None,
+                    },
+                    File {
+                        name: "tzu".to_string(),
+                        meta: None,
+                    }
+                )
+            )
+        );
+
+        let sample3 = "\
+        --- lao 08f78e0addd5bf7b7aa8887e406493e75e8d2b55
+        +++ tzu e044048282ce75186ecc7a214fd3d9ba478a2816\n"
+        .as_bytes();
+        assert_eq!(
+            headers(sample3).unwrap(),
+            (
+                "".as_bytes(),
+                (
+                    File {
+                        name: "lao".to_string(),
+                        meta: Some(FileMetadata::Other(
+                            "08f78e0addd5bf7b7aa8887e406493e75e8d2b55"
+                        )),
+                    },
+                    File {
+                        name: "tzu".to_string(),
+                        meta: Some(FileMetadata::Other(
+                            "e044048282ce75186ecc7a214fd3d9ba478a2816"
+                        )),
+                    }
+                )
+            )
+        );
+    }
+
+    #[test]
+    fn test_range() {
+        assert_eq!(
+            range("1,7".as_bytes()).unwrap(),
+            ("".as_bytes(), Range { start: 1, count: 7 })
+        );
+
+        assert_eq!(
+            range("2".as_bytes()).unwrap(),
+            ("".as_bytes(), Range { start: 2, count: 1 })
+        );
+    }
+
+    #[test]
+    fn test_chunk_intro() {
+        let sample = "@@ -1,7 +1,6 @@\n".as_bytes();
+        assert_eq!(
+            chunk_intro(sample).unwrap(),
+            (
+                "".as_bytes(),
+                (Range { start: 1, count: 7 }, Range { start: 1, count: 6 }),
+            )
+        );
+    }
+
+    #[test]
+    fn test_chunk() {
+        let sample = "\
+        @@ -1,7 +1,6 @@
+        -The Way that can be told of is not the eternal Way;
+        -The name that can be named is not the eternal name.
+        The Nameless is the origin of Heaven and Earth;
+        -The Named is the mother of all things.
+        +The named is the mother of all things.
+        +
+        Therefore let there always be non-being,
+        so we may see their subtlety,
+        And let there always be being,\n"
+        .as_bytes();
+        let expected = Hunk {
+            old_range: Range { start: 1, count: 7 },
+            new_range: Range { start: 1, count: 6 },
+            lines: vec![
             Line::Remove("The Way that can be told of is not the eternal Way;"),
             Line::Remove("The name that can be named is not the eternal name."),
             Line::Context("The Nameless is the origin of Heaven and Earth;"),
@@ -464,82 +468,80 @@ fn test_chunk() {
             Line::Context("Therefore let there always be non-being,"),
             Line::Context("  so we may see their subtlety,"),
             Line::Context("And let there always be being,"),
-        ],
-    };
-    assert_eq!(chunk(sample), IResult::Done("".as_bytes(), expected))
-}
+            ],
+        };
+        assert_eq!(chunk(sample).unwrap(), ("".as_bytes(), expected));
+    }
 
-#[test]
-fn test_patch() {
-    // https://www.gnu.org/software/diffutils/manual/html_node/Example-Unified.html
-    let sample = "\
---- lao 2002-02-21 23:30:39.942229878 -0800
-+++ tzu 2002-02-21 23:30:50.442260588 -0800
-@@ -1,7 +1,6 @@
--The Way that can be told of is not the eternal Way;
--The name that can be named is not the eternal name.
- The Nameless is the origin of Heaven and Earth;
--The Named is the mother of all things.
-+The named is the mother of all things.
-+
- Therefore let there always be non-being,
-   so we may see their subtlety,
- And let there always be being,
-@@ -9,3 +8,6 @@
- The two are the same,
- But after they are produced,
-   they have different names.
-+They both may be called deep and profound.
-+Deeper and more profound,
-+The door of all subtleties!\n";
+    #[test]
+    fn test_patch() {
+        // https://www.gnu.org/software/diffutils/manual/html_node/Example-Unified.html
+        let sample = "\
+        --- lao 2002-02-21 23:30:39.942229878 -0800
+        +++ tzu 2002-02-21 23:30:50.442260588 -0800
+        @@ -1,7 +1,6 @@
+        -The Way that can be told of is not the eternal Way;
+        -The name that can be named is not the eternal name.
+        The Nameless is the origin of Heaven and Earth;
+        -The Named is the mother of all things.
+        +The named is the mother of all things.
+        +
+        Therefore let there always be non-being,
+        so we may see their subtlety,
+        And let there always be being,
+        @@ -9,3 +8,6 @@
+        The two are the same,
+        But after they are produced,
+        they have different names.
+        +They both may be called deep and profound.
+        +Deeper and more profound,
+        +The door of all subtleties!\n";
 
-    let expected = Patch {
-        old: File {
-            name: "lao".to_string(),
-            meta: Some(FileMetadata::DateTime(
-                DateTime::parse_from_rfc3339("2002-02-21T23:30:39.942229878-08:00").unwrap(),
-            )),
-        },
-        new: File {
-            name: "tzu".to_string(),
-            meta: Some(FileMetadata::DateTime(
-                DateTime::parse_from_rfc3339("2002-02-21T23:30:50.442260588-08:00").unwrap(),
-            )),
-        },
-        hunks: vec![
+        let expected = Patch {
+            old: File {
+                name: "lao".to_string(),
+                meta: Some(FileMetadata::DateTime(
+                    DateTime::parse_from_rfc3339("2002-02-21T23:30:39.942229878-08:00").unwrap(),
+                )),
+            },
+            new: File {
+                name: "tzu".to_string(),
+                meta: Some(FileMetadata::DateTime(
+                    DateTime::parse_from_rfc3339("2002-02-21T23:30:50.442260588-08:00").unwrap(),
+                )),
+            },
+            hunks: vec![
             Hunk {
                 old_range: Range { start: 1, count: 7 },
                 new_range: Range { start: 1, count: 6 },
                 lines: vec![
-                    Line::Remove("The Way that can be told of is not the eternal Way;"),
-                    Line::Remove("The name that can be named is not the eternal name."),
-                    Line::Context("The Nameless is the origin of Heaven and Earth;"),
-                    Line::Remove("The Named is the mother of all things."),
-                    Line::Add("The named is the mother of all things."),
-                    Line::Add(""),
-                    Line::Context("Therefore let there always be non-being,"),
-                    Line::Context("  so we may see their subtlety,"),
-                    Line::Context("And let there always be being,"),
+                Line::Remove("The Way that can be told of is not the eternal Way;"),
+                Line::Remove("The name that can be named is not the eternal name."),
+                Line::Context("The Nameless is the origin of Heaven and Earth;"),
+                Line::Remove("The Named is the mother of all things."),
+                Line::Add("The named is the mother of all things."),
+                Line::Add(""),
+                Line::Context("Therefore let there always be non-being,"),
+                Line::Context("  so we may see their subtlety,"),
+                Line::Context("And let there always be being,"),
                 ],
             },
             Hunk {
                 old_range: Range { start: 9, count: 3 },
                 new_range: Range { start: 8, count: 6 },
                 lines: vec![
-                    Line::Context("The two are the same,"),
-                    Line::Context("But after they are produced,"),
-                    Line::Context("  they have different names."),
-                    Line::Add("They both may be called deep and profound."),
-                    Line::Add("Deeper and more profound,"),
-                    Line::Add("The door of all subtleties!"),
+                Line::Context("The two are the same,"),
+                Line::Context("But after they are produced,"),
+                Line::Context("  they have different names."),
+                Line::Add("They both may be called deep and profound."),
+                Line::Add("Deeper and more profound,"),
+                Line::Add("The door of all subtleties!"),
                 ],
             },
-        ],
-        no_newline: true,
-    };
+            ],
+            no_newline: true,
+        };
 
-    assert_eq!(
-        patch(sample.as_bytes()),
-        IResult::Done("".as_bytes(), expected)
-    );
+        assert_eq!(patch(sample.as_bytes()).unwrap(), ("".as_bytes(), expected));
+    }
 }
