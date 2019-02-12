b59683a4230187c113a289f35286261f836745f1
Sunjay Varma <varma.sunjay@gmail.com>
2019-02-12 15:54:01 -0500

    Fixed link formatting in docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-//! patch-rs is a parser library for [Unified Format]
-//! (https://www.gnu.org/software/diffutils/manual/html_node/Unified-Format.html#Unified-Format)
-//! diffs.
+//! patch-rs is a parser library for [Unified Format] diffs.
 //!
-//! GVR also honed down the spec a bit more:
-//! http://www.artima.com/weblogs/viewpost.jsp?thread=164293
+//! More info: Guido van Rossum also [honed down the spec][spec] a bit more.
+//!
+//! [Unified Format]: https://www.gnu.org/software/diffutils/manual/html_node/Unified-Format.html#Unified-Format
+//! [spec]: http://www.artima.com/weblogs/viewpost.jsp?thread=164293
 
 extern crate chrono;
 extern crate nom;
